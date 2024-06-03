006d166ff25c9d9f63b419a65a3e5467bf9dbe8e
LilStpd <4646rc.if@gmail.com>
2024-06-03 22:00:57 +0400

    Initial commit

---
+++ app/src/main/java/com/example/cookingappg/Routes.kt
@@ -0,0 +1,18 @@
+package com.example.cookingappg
+
+object Routes {
+    //main pages
+    val LOGIN = "/login"
+    val REGISTRATION = "/reg"
+    val MENU = "/menu"
+    //menu pages
+    val HOME = "/home"
+    val PLANER = "/planer"
+    val ADD = "/add"
+    val KITCHEN = "/kitchen"
+    val PROFILE = "/user"
+    val FILTERS = "/filters"
+    //other
+    val RECIPE = "/recipe"
+
+}
---
+++ app/src/main/java/com/example/cookingappg/ui/theme/Color.kt
@@ -0,0 +1,20 @@
+package com.example.cookingappg.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+//My Colors
+
+val TextDark = Color(0xFF2E1B09)
+val TextLight = Color(0xFF96876B)
+val Primary = Color(0xFFED9E00)
+val Simple = Color(0xFFF7EDD9)
+val White = Color(0xFFFFFFFF)
+val Gray = Color(0x80E4E4E4)
